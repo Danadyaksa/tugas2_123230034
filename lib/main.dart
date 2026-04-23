1739204c3e3c69150bc1d88bf4e07165d67482af
Danadyaksa <aksnonymous@gmail.com>
2026-04-24 01:09:28 +0700

    anu

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:intl/date_symbol_data_local.dart'; // 1. Tambahkan import ini
 import 'models/transaction.dart';
 import 'screens/main_navigation.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // 2. Inisialisasi data format tanggal untuk locale Indonesia
+  await initializeDateFormatting('id_ID', null);
+
   await Hive.initFlutter();
   Hive.registerAdapter(TransactionAdapter());
   await Hive.openBox<Transaction>('transactions');
@@ -20,10 +25,8 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Smart Expense Tracker',
       theme: ThemeData(
-        // Warna Pastel
-        primaryColor: const Color(0xFFA7C7E7), // Pastel Blue
-        scaffoldBackgroundColor: const Color(0xFFFDFCF0), // Off-white pastel
-        
+        primaryColor: const Color(0xFFA7C7E7),
+        scaffoldBackgroundColor: const Color(0xFFFDFCF0),
         appBarTheme: AppBarTheme(
           backgroundColor: const Color(0xFFA7C7E7),
           foregroundColor: Colors.blueGrey[800],
@@ -34,11 +37,10 @@ class MainApp extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
-        
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color(0xFFA7C7E7),
           primary: const Color(0xFFA7C7E7),
-          secondary: const Color(0xFFC3B1E1), // Pastel Purple
+          secondary: const Color(0xFFC3B1E1),
         ),
       ),
       home: const MainNavigation(),
